4f16b879774e54dec548e3b10ad4e0adf3a93a7b
Bhoomika_Sen <bhoomikasengit@gmail.com>
2022-07-05 22:47:42 +0530

    first commit

---
+++ ScalaProcaQues/src/Logic/que1.scala
@@ -0,0 +1,17 @@
+package Logic
+
+object que1 extends App {
+
+  val s1="hello hi hey"
+  var s3=""
+for(i<-0 to s1.length-1){
+  if(s1(i)=='h'){
+    s3=s1.replace('h','w')
+    //println(s3)
+  }
+
+}
+  println(s3)
+
+
+}
