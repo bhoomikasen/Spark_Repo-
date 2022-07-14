c1a017e047ca9b2370543c325aec51cc2b1ed9aa
Bhoomika_Sen <bhoomikasengit@gmail.com>
2022-07-14 21:42:05 +0530

    added Que3 code

---
+++ ScalaProcaQues/src/Logic/Que3.scala
@@ -0,0 +1,18 @@
+package Logic
+
+object Que3 {
+  def main(args:Array[String]): Unit ={
+    val s1=scala.io.StdIn.readLine("Please enter any string")
+    for(i<-0 to s1.length-1){
+      var count=0
+      for(j<-i to s1.length-1){
+        if(s1(j)==s1(i)){
+          count=count+1
+        }
+      }
+      if(count>1){
+      println(s1(i) +" apearred "+count+" times")}
+    }
+  }
+
+}
